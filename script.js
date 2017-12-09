f3b93f45bbf251cce97d50df6db5ca59d1072da9
Dan Zasiadko <zdaniil92@gmail.com>
2017-12-09 22:13:09 +0200

    my commit for landing

---
+++ script.js
@@ -0,0 +1,7 @@
+$(.'carousel').carousel({
+	interval: 3000
+})
+$('#exampleModal').modal({
+	keyboard: false,
+	backdrop: "static" /*при нажатии на свободную область экрана ничего не будет закрываться*/
+})
